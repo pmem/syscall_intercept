c56b9469ab8a379fcd0cd6e6591a724c705add77
Gabor Buella <gabor.buella@intel.com>
2017-03-02 22:23:57 +0100

    cleanup pull-or-rebuild-image.sh

--- utils/docker/pull-or-rebuild-image.sh
+++ utils/docker/pull-or-rebuild-image.sh
@@ -82,31 +82,6 @@ for file in $files; do echo $file; done
 images_dir_name=images
 base_dir=utils/docker/$images_dir_name
 
-
-
-	# Rebuild Docker image for the current OS version
-	echo "Rebuilding the Docker image for the Dockerfile.$OS-$OS_VER"
-	pushd $images_dir_name
-	./build-image.sh $OS:$OS_VER
-	popd
-
-	# Check if the image has to be pushed to the Docker Hub
-	# (i.e. the build is triggered by commits to the ${DOCKER_USER}/${PROJECT}
-	# repository's master branch, and the Travis build is not
-	# of the "pull_request" type). In that case, create the empty
-	# file.
-	if [[ $DOCKER_PUSH == "1" \
-		&& $MAKE_PKG == "1" ]]
-	then
-		echo "The image will be pushed to the Docker Hub"
-		touch push_image_to_repo_flag
-	else
-		echo "Skip pushing the image to the Docker Hub"
-	fi
-	exit 0
-
-
-
 # Check if committed file modifications require the Docker image to be rebuilt
 for file in $files; do
 	# Check if modified files are relevant to the current build
