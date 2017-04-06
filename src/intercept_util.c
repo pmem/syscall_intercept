e7993579dbbd5a86ead9018e2ec3ff26b0d570b3
Marcin Ślusarz <marcin.slusarz@intel.com>
2017-04-06 16:52:31 +0200

    Remove flags argument from fchmodat logger.
    It doesn't exist in kernel interface.

--- src/intercept_util.c
+++ src/intercept_util.c
@@ -895,11 +895,10 @@ intercept_log_syscall(const char *libpath, long nr, long arg0, long arg1,
 				F_DEC, arg3,
 				result_known, result);
 	} else if (nr == SYS_fchmodat) {
-		buf = print_syscall(buf, "fchmodat", 4,
+		buf = print_syscall(buf, "fchmodat", 3,
 				F_DEC, arg0,
 				F_STR, arg1,
 				F_OCT_MODE, arg2,
-				F_DEC, arg3,
 				result_known, result);
 	} else if (nr == SYS_faccessat) {
 		buf = print_syscall(buf, "faccessat", 4,
