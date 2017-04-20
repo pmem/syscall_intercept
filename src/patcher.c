38384615d30992759a6a75a08f665c2fc29baf95
Gabor Buella <gabor.buella@intel.com>
2017-04-20 15:49:50 +0200

    Add some test for failed patching

--- src/patcher.c
+++ src/patcher.c
@@ -335,12 +335,14 @@ create_patch_wrappers(struct intercept_desc *desc)
 			    patch->preceding_ins.is_call ||
 			    patch->preceding_ins.is_jump ||
 			    patch->preceding_ins.is_ret ||
+			    patch->preceding_ins.is_syscall ||
 			    has_jump(desc, patch->syscall_addr));
 
 			patch->uses_prev_ins_2 = patch->uses_prev_ins &&
 			    !(patch->preceding_ins_2.has_ip_relative_opr ||
 			    patch->preceding_ins_2.is_call ||
 			    patch->preceding_ins_2.is_ret ||
+			    patch->preceding_ins_2.is_syscall ||
 			    has_jump(desc, patch->syscall_addr
 				- patch->preceding_ins.length));
 
@@ -348,6 +350,7 @@ create_patch_wrappers(struct intercept_desc *desc)
 			    !(patch->following_ins.has_ip_relative_opr ||
 			    patch->following_ins.is_rel_jump ||
 			    patch->following_ins.is_call ||
+			    patch->following_ins.is_syscall ||
 			    has_jump(desc,
 				patch->syscall_addr + SYSCALL_INS_SIZE));
 
